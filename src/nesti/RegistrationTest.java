8eb8af8d951f26159282116cb87200d07c6292af
ste <s.cadeck@gmail.com>
2021-05-19 17:22:48 +0200

    Update testUnit

--- src/nesti/RegistrationTest.java
+++ src/nesti/RegistrationTest.java
@@ -2,6 +2,8 @@ package nesti;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
 import org.junit.Test;
 
 public class RegistrationTest {
@@ -11,5 +13,17 @@ public class RegistrationTest {
 		Registration registration = new Registration();
 		assertTrue(registration.checkEmail("steph@example.com"));
 		assertFalse(registration.checkEmail("steph&example.com"));
+	}
+	
+	@Test
+	public void testCheckForceMdp() {
+		Registration registration = new Registration();
+
+		assertEquals(registration.checkForceMdp(
+				"DURAND%%%+++65657777777777777777777777777777777777777////////////////////////////////////////"),
+				Color.GREEN);
+		assertEquals(registration.checkForceMdp("durand123777"), Color.YELLOW);
+		assertEquals(registration.checkForceMdp("durand123"), Color.ORANGE);
+		assertEquals(registration.checkForceMdp("jean"), Color.RED);
 	}
 }
